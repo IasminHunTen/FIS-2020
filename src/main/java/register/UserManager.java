e6c4cd083d6568c11a41b9512a5517d3a422c19d
Vlad Morariu <vlad.morariu99@gmail.com>
2020-05-21 18:03:52 +0300

    Register_3

--- src/main/java/register/UserManager.java
+++ src/main/java/register/UserManager.java
@@ -1,9 +1,47 @@
 package register;
 
-public class UserManager {
+import java.io.File;
+import java.io.IOException;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class UserManager {
+	private ObjectMapper oMap; 
+	
 	public UserManager() {
-		// TODO Auto-generated constructor stub
+		super();
+		this.oMap=new ObjectMapper();
 	}
 
+	public void writeClients(ClientList cList) {
+		try {
+			oMap.writerWithDefaultPrettyPrinter().writeValue(new File("client_database.json"), cList);
+		} catch (JsonGenerationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
+	}
+	
+	public void writeAdmins(AdminList aList) {
+		try {
+			oMap.writerWithDefaultPrettyPrinter().writeValue(new File("admin_database.json"), aList);
+		} catch (JsonGenerationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
+	}
 }
